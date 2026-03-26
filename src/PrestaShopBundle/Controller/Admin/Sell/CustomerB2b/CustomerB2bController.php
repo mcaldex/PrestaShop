36c7125fb8dc6c766643c3c96a632769143d58c5
Soledis - Clément Hervouet <clem.hervouet@live.fr>
2026-03-26 15:34:58 +0100

    add Customers B2B section with new controller, routing, and tab configuration. Add icon for Business Entity and fix retrieval of current tab in NavBar

--- src/PrestaShopBundle/Controller/Admin/Sell/CustomerB2b/CustomerB2bController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/CustomerB2b/CustomerB2bController.php
@@ -4,20 +4,20 @@
  * docs/licenses/LICENSE.txt file that was distributed with this source code.
  */
 
-namespace PrestaShopBundle\Controller\Admin\Sell\BusinessEntity;
+namespace PrestaShopBundle\Controller\Admin\Sell\CustomerB2b;
 
 use PrestaShopBundle\Controller\Admin\PrestaShopAdminController;
 use PrestaShopBundle\Security\Attribute\AdminSecurity;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Class BusinessEntitiesController manages the "Sell > Business Entities" page.
+ * Class CustomerB2bController manages the "Sell > Customers B2B" page.
  */
-class BusinessEntitiesController extends PrestaShopAdminController
+class CustomerB2bController extends PrestaShopAdminController
 {
-    #[AdminSecurity("is_granted('read', 'AdminBusinessEntities')")]
+    #[AdminSecurity("is_granted('read', 'AdminCustomersB2b')")]
     public function listAction(): Response
     {
-        return $this->render('@PrestaShop/Admin/Sell/BusinessEntity/list.html.twig');
+        return $this->render('@PrestaShop/Admin/Sell/CustomerB2b/list.html.twig');
     }
 }
